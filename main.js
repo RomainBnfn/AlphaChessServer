e5897736a8721f1e81145b336de8746dbaa6c88a
RomainBnfn <61451498+RomainBnfn@users.noreply.github.com>
2021-04-25 21:56:12 +0200

    Game

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//#region HTTP & Socket Connexion
 const http = require("http");
 
 const hostname = "localhost";
@@ -12,41 +13,167 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-//const app = require("express");
-//const httpServer = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
   cors: true,
   origins: ["*"],
 });
+//#endregion
+
+// firebaseUID -> { socketId : socketId, displayName : string, opponentFirebaseUID : string | null }
+// Car l'UID de firebase est unique pour chaque compte alors que le
+// socketID ne l'est que pour une connexion
+const infoJoueurs = new Map();
+
+//#region Utilitaires
+setVarValue = (firebaseUID, variable, value) => {
+  let infos = infoJoueurs.get(firebaseUID);
+  if (!infos) {
+    infos = {};
+  }
+  infos[variable] = value;
+  infoJoueurs.set(firebaseUID, infos);
+};
+
+getVarValue = (firebaseUID, variable) => {
+  let infos = infoJoueurs.get(firebaseUID);
+  if (!infos) {
+    return null;
+  }
+  return infos[variable];
+};
+
+setPseudo = (firebaseUID, pseudo) => {
+  setVarValue(firebaseUID, "pseudo", pseudo);
+};
+
+setOpponentFirebaseUID = (firebaseUID, opponentFirebaseUID) => {
+  setVarValue(firebaseUID, "opponentFirebaseUID", opponentFirebaseUID);
+};
+
+getOpponentFirebaseUID = (firebaseUID) => {
+  return getVarValue(firebaseUID, "opponentFirebaseUID");
+};
+
+setSocketID = (firebaseUID, socketId) => {
+  let oldSocketId = getVarValue(firebaseUID, "socketID");
+  if (oldSocketId && oldSocketId != socketId) {
+    // L'utilisateur était déjà connecté (autre onglet ?)
+    io.to(oldSocketId).emit("close", {});
+  }
+  setVarValue(firebaseUID, "socketID", socketId);
+};
+
+getSocketID = (firebaseUID) => {
+  return getVarValue(firebaseUID, "socketID");
+};
+//#endregion
+
+sendOpponentList = (socketID) => {
+  let opponents = [];
+  for (let [key, value] of infoJoueurs) {
+    opponents.push({
+      firebaseUID: key,
+      pseudo: value.pseudo,
+      opponent: value.opponent,
+    });
+  }
+  io.to(socketID).emit("listOpponent", { opponents: opponents });
+};
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  // Infos de l'utilisateur du socket
+  let socketId = socket.id;
+  let firebaseUID = "";
+  let pseudo = "";
 
-  socket.emit("connected", {});
+  let opponentFirebaseUID = null;
+  let opponentSocketID = null;
 
-  socket.on("test", (data) => {
-    console.log("Test recieved");
-    console.log(data);
+  socket.on("initConnexion", (data) => {
+    pseudo = data.pseudo;
+    firebaseUID = data.firebaseUID;
+    setPseudo(firebaseUID, pseudo);
+    setSocketID(firebaseUID, socketId);
+    console.log("[+] " + pseudo + " s'est connecté.");
+
+    sendOpponentList(socketId);
+    // Envoie au nouvel utilisateur de la liste des anciens joueurs en ligne
+
+    io.emit("newConnexion", {
+      opponent: { pseudo: pseudo, firebaseUID: firebaseUID },
+    });
+    // Envoie aux anciens du nouvel utilisateur
   });
 
-  socket.on("askForPseudo", (data) => {
-    console.log(socket.id + " ask for pseudo : " + data.pseudo);
-    socket.emit("pseudoResponse", { response: true });
+  socket.on("changePseudo", (data) => {
+    pseudo = data.pseudo;
+    setPseudo(firebaseUID, pseudo);
+
+    io.emit("changePseudo", {
+      opponent: { pseudo: pseudo, firebaseUID: firebaseUID },
+    });
+  });
+
+  socket.on("demandeDuel", (data) => {
+    let opponent = data.opponent;
+    let socketOpponent = getVarValue(opponent.firebaseUID, "socketID");
+    io.to(socketOpponent).emit("demandeDuel", {
+      opponent: { pseudo: pseudo, firebaseUID: firebaseUID },
+    });
+  });
+
+  socket.on("accepterDuel", (data) => {
+    let opponent = data.opponent;
+    opponentFirebaseUID = opponent.firebaseUID;
+    opponentSocketID = getSocketID(opponentFirebaseUID);
+
+    io.to(opponentSocketID).emit("acceptationDuel", {
+      opponent: { pseudo: pseudo, firebaseUID: firebaseUID },
+    });
+
+    setOpponentFirebaseUID(firebaseUID, opponentFirebaseUID);
+    setOpponentFirebaseUID(opponentFirebaseUID, firebaseUID);
+
+    // todo
+    io.to(opponentSocketID).emit("startGame", {
+      myTurn: false,
+    });
+    io.to(socketId).emit("startGame", {
+      myTurn: true,
+    });
+  });
+
+  socket.on("turn", (data) => {
+    if (!opponentFirebaseUID) {
+      opponentFirebaseUID = getOpponentFirebaseUID(firebaseUID);
+      opponentSocketID = getSocketID(opponentFirebaseUID);
+    }
+    let piece = data.piece;
+    let pos = data.pos;
+    io.to(opponentSocketID).emit("turn", { piece: piece, pos: pos });
+  });
+
+  socket.on("refuserDuel", (data) => {
+    let opponent = data.opponent;
+    let socketOpponent = getSocketID(opponent.firebaseUID);
+    io.to(socketOpponent).emit("refusDuel", {
+      opponent: { pseudo: pseudo, firebaseUID: firebaseUID },
+    });
   });
 
   //socket.broadcast.emit("message", "A new personne is connected !");
-
   //io.emit('message', 'This a send message')
   //socket.join("UniqueIDForTheRoom");
   //socket.to("Unique ID").emit("message", "Blabla");
   //io.to("Unique ID").emit("message", "data");
-});
 
-//const PORT = process.env.PORT || 3000;
+  socket.on("disconnect", (data) => {
+    infoJoueurs.delete(firebaseUID);
+    console.log("[-] " + pseudo + " s'est déconnecté.");
 
-/*
-httpServer.listen(PORT, () => {
-  console.log("Server ruing on port " + PORT);
+    io.emit("newDisconnexion", {
+      opponent: { pseudo: pseudo, firebaseUID: firebaseUID },
+    });
+  });
 });
-*/
